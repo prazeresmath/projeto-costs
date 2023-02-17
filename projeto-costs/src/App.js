531dec0b747c5ef53b21f0226a128ce2823af078
prazeresmath <matheus.prazeres@hotmail.com>
2023-02-17 12:30:42 -0300

    criando as telas

--- projeto-costs/src/App.js
+++ projeto-costs/src/App.js
@@ -1,10 +1,30 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contato from './components/pages/Contato';
+import NewProject from './components/pages/NewProject';
 
+import Container from './components/layout/Container';
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contato">Contato</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <p>footer</p>
+    </Router>
   );
 }
 
