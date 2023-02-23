6e1c41738a37c5af423e91e495245a04c73d29cf
prazeresmath <matheus.prazeres@hotmail.com>
2023-02-23 15:13:56 -0300

    criação do home e do newproject

--- projeto-costs/src/components/pages/NewProject.js
+++ projeto-costs/src/components/pages/NewProject.js
@@ -1,7 +1,13 @@
+import styles from './NewProject.module.css';
+import ProjectForm from '../project/ProjectForm';
+
+
 function NewProject() {
     return (
-        <div>
-            <h1>New project</h1>
+        <div className={styles.newproject_container}>
+            <h1>Criar projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm btnText="Criar projeto"/>
         </div>
     )
 }
