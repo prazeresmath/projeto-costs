8f9dc6f966fc53ab94e9afe31a4007c0a7ada2c5
prazeresmath <matheus.prazeres@hotmail.com>
2023-02-24 14:29:50 -0300

    inserindo dados via API

--- projeto-costs/src/components/pages/NewProject.js
+++ projeto-costs/src/components/pages/NewProject.js
@@ -1,13 +1,39 @@
-import styles from './NewProject.module.css';
+import { useNavigate } from 'react-router-dom';
+
 import ProjectForm from '../project/ProjectForm';
 
+import styles from './NewProject.module.css';
 
 function NewProject() {
-    return (
+
+    const history = useNavigate()
+
+    function createPost(project) {
+
+        // initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch('http://localhost:3001/projects', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp) => resp.json)
+        .then((data) => {
+            console.log(data)
+            history('/Projects', {message: 'Projeto criado com sucesso!'})
+        })
+        .catch(err => console.log(err))
+    }
+    
+    return(
         <div className={styles.newproject_container}>
             <h1>Criar projeto</h1>
-            <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar projeto"/>
+            <p>crie seu projeto pra depois adicionar os serviços</p>
+            <ProjectForm handleSubmit={createPost} btnText="criar projeto"/>
         </div>
     )
 }
